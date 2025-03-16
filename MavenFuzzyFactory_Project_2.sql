cd0bc517faf8c97d8bdd742b583def676c069103
andrewwithcode <163968186+andrewwithcode@users.noreply.github.com>
2025-03-16 20:09:23 +0700

    Update MavenFuzzyFactory_Project_2.sql

--- MavenFuzzyFactory_Project_2.sql
+++ MavenFuzzyFactory_Project_2.sql
@@ -1,9 +1,8 @@
 USE mavenfuzzyfactory;
 
 /*
-1. First, I’d like to show our volume growth. Can you pull overall session and order volume, 
-trended by quarter for the life of the business? Since the most recent quarter is incomplete, 
-you can decide how to handle it.
+1. First, I’d like to show our volume growth. I will pull the overall session and order volume, 
+trended by quarter for the life of the business?
 */ 
 
 SELECT 
@@ -39,7 +38,7 @@ ORDER BY 1,2
 
 
 /*
-3. I’d like to show how we’ve grown specific channels. Could you pull a quarterly view of orders 
+3. I’d like to show how we’ve grown specific channels. I will pull a quarterly view of orders 
 from Gsearch nonbrand, Bsearch nonbrand, brand search overall, organic search, and direct type-in?
 */
 
@@ -63,7 +62,7 @@ ORDER BY 1,2
 
 /*
 4. Next, let’s show the overall session-to-order conversion rate trends for those same channels, 
-by quarter. Please also make a note of any periods where we made major improvements or optimizations.
+by quarter.Also make a note of any periods where we made major improvements or optimizations.
 */
 
 SELECT 
@@ -89,7 +88,7 @@ ORDER BY 1,2
 
 /*
 5. We’ve come a long way since the days of selling a single product. Let’s pull monthly trending for revenue 
-and margin by product, along with total sales and revenue. Note anything you notice about seasonality.
+and margin by product, along with total sales and revenue. Note anything interesting about seasonality.
 */
 
 
@@ -113,7 +112,7 @@ ORDER BY 1,2
 
 
 /*
-6. Let’s dive deeper into the impact of introducing new products. Please pull monthly sessions to 
+6. Let’s dive deeper into the impact of introducing new products. I will pull monthly sessions to 
 the /products page, and show how the % of those sessions clicking through another page has changed 
 over time, along with a view of how conversion from /products to placing an order has improved.
 */
@@ -149,7 +148,7 @@ GROUP BY 1,2
 
 /*
 7. We made our 4th product available as a primary product on December 05, 2014 (it was previously only a cross-sell item). 
-Could you please pull sales data since then, and show how well each product cross-sells from one another?
+I will pull sales data since then, and show how well each product cross-sells from one another
 */
 
 CREATE TEMPORARY TABLE primary_products
@@ -196,10 +195,6 @@ FROM primary_products
 GROUP BY 1;
 
 
-/*
-8. In addition to telling investors about what we’ve already achieved, let’s show them that we still have plenty 
-of gas in the tank. Based on all the analysis you’ve done, could you share some recommendations and 
-opportunities for us going forward? No right or wrong answer here – I’d just like to hear your perspective!
-*/
+
 
 
