48b14dac977f0570c7bc70bd9901af9fb95fd144
andrewwithcode <163968186+andrewwithcode@users.noreply.github.com>
2025-03-16 20:12:23 +0700

    Update MavenFuzzyFactory_Project_1.sql

--- MavenFuzzyFactory_Project_1.sql
+++ MavenFuzzyFactory_Project_1.sql
@@ -2,8 +2,8 @@ use mavenfuzzyfactory;
 
 
 /*
-1.	Gsearch seems to be the biggest driver of our business. Could you pull monthly 
-trends for gsearch sessions and orders so that we can showcase the growth there? 
+1.	Gsearch seems to be the biggest driver of our business. I will monthly 
+trends for gsearch sessions and orders so that we can showcase the growth there
 */ 
 
 SELECT
@@ -40,7 +40,7 @@ GROUP BY 1,2;
 
 
 /*
-3.	While we’re on Gsearch, could you dive into nonbrand, and pull monthly sessions and orders split by device type? 
+3.	While we’re on Gsearch, I will dive into nonbrand, and pull monthly sessions and orders split by device type? 
 I want to flex our analytical muscles a little and show the board we really know our traffic sources. 
 */ 
 
@@ -63,7 +63,7 @@ GROUP BY 1,2;
 
 /*
 4.	I’m worried that one of our more pessimistic board members may be concerned about the large % of traffic from Gsearch. 
-Can you pull monthly trends for Gsearch, alongside monthly trends for each of our other channels?
+I will monthly trends for Gsearch, alongside monthly trends for each of our other channels
 */ 
 
 -- first, finding the various utm sources and referers to see the traffic we're getting
@@ -92,7 +92,7 @@ GROUP BY 1,2;
 
 /*
 5.	I’d like to tell the story of our website performance improvements over the course of the first 8 months. 
-Could you pull session to order conversion rates, by month? 
+I will pull session to order conversion rates, by month
 
 */ 
 
@@ -110,8 +110,8 @@ GROUP BY 1,2;
 
 /*
 6.	For the gsearch lander test, please estimate the revenue that test earned us 
-(Hint: Look at the increase in CVR from the test (Jun 19 – Jul 28), and use 
-nonbrand sessions and revenue since then to calculate incremental value)
+Look at the increase in CVR from the test (Jun 19 – Jul 28), and use 
+nonbrand sessions and revenue since then to calculate incremental value
 */ 
 
 USE mavenfuzzyfactory;
@@ -208,7 +208,7 @@ WHERE created_at < '2012-11-27'
 
 /*
 7.	For the landing page test you analyzed previously, it would be great to show a full conversion funnel 
-from each of the two pages to orders. You can use the same time period you analyzed last time (Jun 19 – Jul 28).
+from each of the two pages to orders. I will use the same time-period you analyzed last time (Jun 19 – Jul 28).
 */ 
 
 SELECT
@@ -321,7 +321,7 @@ GROUP BY 1
 
 
 /*
-8.	I’d love for you to quantify the impact of our billing test, as well. Please analyze the lift generated 
+8.	I’d love for you to quantify the impact of our billing test, as well. I will analyze the lift generated 
 from the test (Sep 10 – Nov 10), in terms of revenue per billing page session, and then pull the number 
 of billing page sessions for the past month to understand monthly impact.
 */ 
